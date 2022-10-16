ce16499887c0ae30cbb3f932e460f015c957bb12
corniab <bencornia@byui.edu>
2022-10-15 20:44:47 -0600

    Add log statement for server.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Listen on port 5000
 app.listen(5000, () => {
-  console.log("server listening at port 5000");
+  console.log("⚡ [ server ] Listening at port 5000...");
 });
